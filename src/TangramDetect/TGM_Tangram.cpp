86324777b4ffcd72f991cabafbca31a7380d7d4f
Jiantao Wu <jiantaowu.xining@gmail.com>
2012-09-11 00:28:41 -0400

    Change the process of locating the bam index files according to Derek's suggestion

--- src/TangramDetect/TGM_Tangram.cpp
+++ src/TangramDetect/TGM_Tangram.cpp
@@ -36,40 +36,56 @@ using namespace BamTools;
 
 int main(int argc, char *argv[])
 {
+    // load the command line arguments
     DetectPars detectPars;
     AlignerPars alignerPars;
     Parameters parameters(detectPars, alignerPars);
     parameters.Set((const char**) argv, argc);
 
+    // load the input bam file names
     vector<string> filenames;
     parameters.SetBamFilenames(filenames);
 
+    // open the bam files with bam multi reader
     BamMultiReader bamMultiReader;
     bamMultiReader.Open(filenames);
-    bamMultiReader.LocateIndexes();
 
+    // make sure we have the index files at with the input bam files
+    if (!bamMultiReader.LocateIndexes())
+    {
+        // fail to locate the index files
+        // we need to create them if necessary
+        if (!bamMultiReader.CreateIndexes()) 
+            TGM_ErrQuit("ERROR: Cannot locate or create the index files for the input bam files.\n");
+    }
+
+    // read the library information table
     LibTable libTable;
     libTable.Read(detectPars.fpLibInput, detectPars.maxFragDiff);
 
+    // read the fragment length distribution table
     FragLenTable fragLenTable;
     fragLenTable.Read(detectPars.fpHistInput);
 
+    // where should we start to call the SV events
     parameters.ParseRangeStr(bamMultiReader);
     parameters.SetRange(bamMultiReader, libTable.GetFragLenMax());
 
     BamPairTable bamPairTable(libTable, fragLenTable, detectPars.minSoftSize, detectPars.minMQ, detectPars.spMinMQ);
 
+    // iterate through the bam files and fill the bam pair table
     BamAlignment alignment;
     while(bamMultiReader.GetNextAlignment(alignment))
     {
         bamPairTable.Update(alignment);
     }
 
+    // clean the fragnment length table
     fragLenTable.Destory();
 
+    // call the SV events with read-pair signal
     Detector detector(detectPars, libTable, bamPairTable);
     detector.Init();
-
     detector.CallEvents();
 
     const Reference* pRef = NULL;
@@ -78,6 +94,7 @@ int main(int argc, char *argv[])
     Reference reference;
     Aligner aligner(detector, bamPairTable,  alignerPars, reference, libTable);
 
+    // call the SV events with split-read signal
     if (alignerPars.fpRefInput != NULL)
     {
         // always load the whole chromosome reference
@@ -90,9 +107,11 @@ int main(int argc, char *argv[])
         pAligner = &aligner;
     }
 
+    // print out the events (vcf format)
     Printer printer(&detector, detectPars, pAligner, pRef, libTable, bamPairTable);
     printer.Print();
 
+    // clean up and quit
     bamMultiReader.Close();
     return EXIT_SUCCESS;
 }
